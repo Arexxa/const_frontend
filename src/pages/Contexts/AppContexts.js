7895e1d529a0a011a00fdf20b8f13775e47ae589
XL226261 <ahazxa@syntronic.com>
2024-04-25 02:30:10 +0800

    Update profile

--- src/pages/Contexts/AppContexts.js
+++ src/pages/Contexts/AppContexts.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { LoginProvider } from '../Auth/Login/contexts/LoginContext'
 import { RegisterProvider } from '../Auth/Register/contexts/RegisterContext'
 import { UserManagementProvider } from '../admin/usermanagement/contexts/UserManagementContext'
+import { ProfileProvider } from '../Profile/contexts/ProfileContexts'
 
 const AppContextProviders = ({ children }) => {
     return (
-<LoginProvider>
+        <LoginProvider>
             <RegisterProvider>
-                <UserManagementProvider>{children}</UserManagementProvider>
+                <ProfileProvider>
+                    <UserManagementProvider>{children}</UserManagementProvider>
+                </ProfileProvider>
             </RegisterProvider>
         </LoginProvider>
     )
